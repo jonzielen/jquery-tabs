e8394f8fc4db23aa2c0df6b5da481c312cfc9f33
Jon Zielenkievicz <jonzielen@gmail.com>
2013-12-01 19:25:50 -0500

    add files for tabs

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function() {
+	/*jquery tabs*/
+	var lastClick = 0;
+
+	$('#tab-nav li a').click(function(e) {
+		var tabNumber = $(this).parent().index();
+
+		if (tabNumber !== lastClick) {
+			lastClick = tabNumber;
+			$('#tab-nav li a').removeClass('active-tab');
+			$(this).addClass('active-tab');
+			$('#tab-holder div').fadeOut();
+			$('#tab-holder div:eq('+tabNumber+')').fadeIn();
+			var containerHeight = $('#tab-nav').innerHeight() + $('#tab-holder div:eq('+tabNumber+')').innerHeight();
+			$('#tab-container').animate({
+				height: containerHeight
+			});
+			e.preventDefault();
+		} else {
+			return false;
+		};
+	});
+});

4a8a461f328aaf7972c4ec97f657dc0b32e277d1
Jon Zielenkievicz <jonzielen@gmail.com>
2013-12-02 22:43:12 -0500

    update jquery tabs demo page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,23 @@
-console.log('This would be the main JS file.');
+$(function() {
+	/*jquery tabs*/
+	var lastClick = 0;
+
+	$('#tab-nav li a').click(function(e) {
+		var tabNumber = $(this).parent().index();
+
+		if (tabNumber !== lastClick) {
+			lastClick = tabNumber;
+			$('#tab-nav li a').removeClass('active-tab');
+			$(this).addClass('active-tab');
+			$('#tab-holder div').fadeOut();
+			$('#tab-holder div:eq('+tabNumber+')').fadeIn();
+			var containerHeight = $('#tab-nav').innerHeight() + $('#tab-holder div:eq('+tabNumber+')').innerHeight();
+			$('#tab-container').animate({
+				height: containerHeight
+			});
+			e.preventDefault();
+		} else {
+			return false;
+		};
+	});
+});
